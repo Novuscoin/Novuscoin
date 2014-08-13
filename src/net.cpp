479c543ca37dc8a0cd0f9e928e4ee03af73742a1
Novuscoin <webmaster@novuscoin.com>
2014-08-13 18:18:06 -0400

    Novus Coin, Corp. Novuscoin version.

--- src/net.cpp
+++ src/net.cpp
@@ -1191,9 +1191,10 @@ void MapPort(bool)
 // Each pair gives a source name and a seed name.
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
+//I was not sure what I should do about Xurious.com and wemine-testnet.com if I should setup something similar
 static const char *strMainNetDNSSeed[][2] = {
-    {"litecointools.com", "dnsseed.litecointools.com"},
-    {"litecoinpool.org", "dnsseed.litecoinpool.org"},
+    {"novuscointools.com", "dnsseed.novuscointools.com"},
+    {"novuscoinpool.org", "dnsseed.novuscoinpool.org"},
     {"xurious.com", "dnsseed.ltc.xurious.com"},
     {"koin-project.com", "dnsseed.koin-project.com"},
     {"weminemnc.com", "dnsseed.weminemnc.com"},
@@ -1201,8 +1202,10 @@ static const char *strMainNetDNSSeed[][2] = {
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"litecointools.com", "testnet-seed.litecointools.com"},
-    {"xurious.com", "testnet-seed.ltc.xurious.com"},
+//I setup on the Godaddy server but I am not sure I did it right, Giovanni.
+//I was not sure what I should do about Xurious.com and wemine-testnet.com if I should setup something similar
+    {"novuscointools.com", "testnet-seed.novuscointools.com"},
+    {"xurious.com", "testnet-seed.nov.xurious.com"},
     {"wemine-testnet.com", "dnsseed.wemine-testnet.com"},
     {NULL, NULL}
 };
